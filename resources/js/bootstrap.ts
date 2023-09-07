197715fe802939b7a0c5eea343e757edd37544fb
Nahian27 <59230592+Nahian27@users.noreply.github.com>
2023-09-08 03:22:19 +0600

    forced https

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -8,7 +8,6 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-axios.defaults.baseURL = process.env.APP_URL || 'http://localhost:8000';
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
